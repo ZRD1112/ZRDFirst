4a242f36366ae7496ab7b10acf7bb589ec91a16a
ZRD <2250221621@qq.com>
2018-07-14 10:28:21 +0800

    ZPullF

---
+++ ZRD1112/ZRD1112Tests/ZRD1112Tests.m
@@ -0,0 +1,39 @@
+//
+//  ZRD1112Tests.m
+//  ZRD1112Tests
+//
+//  Created by 公司的mini on 2018/7/14.
+//  Copyright © 2018年 tongyi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZRD1112Tests : XCTestCase
+
+@end
+
+@implementation ZRD1112Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
